1495c6e1368be7a026f04dc89bb77c111809fae2
Reynaldo Jr. G. Abrigo <abrigo.reynaldobscs2024@gmail.com>
2026-08-16 21:43:56 +0800

    convert header and money to Typescript

--- ecommerce-abrigo/src/components/Header.tsx
+++ ecommerce-abrigo/src/components/Header.tsx
@@ -1,7 +1,15 @@
 import { Link } from 'react-router';
 import './header.css';
 
-export function Header({ cart }) {
+type HeaderProps = {
+  cart: {
+    productId: string;
+    quantity: number;
+    deliveryOptionId: string;
+  }[];
+};
+
+export function Header({ cart }: HeaderProps) {
   let totalQuantity = 0;
 
   cart.forEach((cartItem) => {
@@ -13,33 +21,27 @@ export function Header({ cart }) {
       <div className="left-section">
         <Link to="/" className="header-link">
           <img className="logo"
-            data-testid="header-logo"
             src="images/logo-white.png" />
           <img className="mobile-logo"
-            data-testid="header-mobile-logo"
             src="images/mobile-logo-white.png" />
         </Link>
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search"
-          data-testid="header-search-bar" />
+        <input className="search-bar" type="text" placeholder="Search" />
 
-        <button className="search-button"
-          data-testid="header-search-button">
+        <button className="search-button">
           <img className="search-icon" src="images/icons/search-icon.png" />
         </button>
       </div>
 
       <div className="right-section">
-        <Link className="orders-link header-link" to="/orders"
-          data-testid="header-orders-link">
+        <Link className="orders-link header-link" to="/orders">
 
           <span className="orders-text">Orders</span>
         </Link>
 
-        <Link className="cart-link header-link" to="/checkout"
-          data-testid="header-cart-link">
+        <Link className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
