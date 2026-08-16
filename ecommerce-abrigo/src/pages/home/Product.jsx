0a6f653cac8bb682cc7800e8d6c0bb13ebeafa80
Reynaldo Jr. G. Abrigo <abrigo.reynaldobscs2024@gmail.com>
2026-08-16 18:10:26 +0800

    Integrated test on all pages, components, header, and utils

--- ecommerce-abrigo/src/pages/home/Product.jsx
+++ ecommerce-abrigo/src/pages/home/Product.jsx
@@ -19,9 +19,11 @@ export function Product({ product, loadCart }) {
   };
 
   return (
-    <div className="product-container">
+    <div className="product-container"
+      data-testid="product-container">
       <div className="product-image-container">
         <img className="product-image"
+          data-testid="product-image"
           src={product.image} />
       </div>
 
@@ -31,6 +33,7 @@ export function Product({ product, loadCart }) {
 
       <div className="product-rating-container">
         <img className="product-rating-stars"
+          data-testid="product-rating-stars-image"
           src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
         <div className="product-rating-count link-primary">
           {product.rating.count}
@@ -64,6 +67,7 @@ export function Product({ product, loadCart }) {
       </div>
 
       <button className="add-to-cart-button button-primary"
+        data-testid="add-to-cart-button"
         onClick={addToCart}>
         Add to Cart
       </button>
